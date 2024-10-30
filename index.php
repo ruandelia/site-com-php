e4c7fff63952bd53344c683ab00e27cddfcd871b
ruandelia <ruandelia2@gmail.com>
2024-10-30 14:17:28 -0300

    html da primeira página

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site</title>
+</head>
+<body>
+    <header>
+        <h1>Site exemplo</h1>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Equipe</a>
+            <a href="">Serviços</a>
+        </nav>
+    </header>
+
+    <main>
+        <article>
+            <h2>Bem-vindo(a)!</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quaerat cum archite
+            </p>
+        </article>
+    </main>
+
+    <footer>
+        <h2>Site criado como exemplo.</h2>
+    </footer>
+</body>
+</html>
